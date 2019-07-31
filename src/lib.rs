5a1c5cec403cb29c96a60594dfaa8e2758ceb58b
Kim Hermansson <k.hermansson@adnymics.com>
2019-07-31 14:01:57 +0200

    Ran cargo fmt. Added a docstring.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use serde::Deserialize;
 use reqwest::Client;
+use serde::Deserialize;
 
 use reqwest::Error as ReqwestError;
 use serde_json::error::Error as JsonError;
@@ -31,23 +31,27 @@ impl GitHubApi {
             .header("Accept", "application/vnd.github.v3+json")
             .basic_auth(&self.username, Some(&self.password))
             .send()
-            {
-                Ok(mut response) => match response.text() {
-                    Ok(text) => Ok(text),
-                    Err(error) => Err(GitHubApiError::ReqwestError(error)),
-                }
-                Err(error) => Err(GitHubApiError::ReqwestError(error))
-            }
+        {
+            Ok(mut response) => match response.text() {
+                Ok(text) => Ok(text),
+                Err(error) => Err(GitHubApiError::ReqwestError(error)),
+            },
+            Err(error) => Err(GitHubApiError::ReqwestError(error)),
+        }
     }
 
-    pub fn get_pull_requests(&self, owner: &str, repository: &str) -> Result<Vec<PullRequest>, GitHubApiError> {
+    pub fn get_pull_requests(
+        &self,
+        owner: &str,
+        repository: &str,
+    ) -> Result<Vec<PullRequest>, GitHubApiError> {
         let method = format!("repos/{}/{}/pulls", owner, repository);
 
         let result = self.api_get_call(&method)?;
 
         match serde_json::from_str(&result) {
             Ok(value) => Ok(value),
-            Err(error) => Err(GitHubApiError::JsonError(error))
+            Err(error) => Err(GitHubApiError::JsonError(error)),
         }
     }
 }
